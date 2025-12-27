f1a17dd5dd6e845cfb01763fff20cc0d64e2b2bd
Aryan Singh <aryan.singhm00@gmail.com>
2025-12-27 11:37:29 +0530

    added gallery page

---
+++ theme.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.getElementById('theme-toggle');
+    const htmlElement = document.documentElement;
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      htmlElement.setAttribute('data-theme', savedTheme);
+    }
+
+    toggleBtn.addEventListener('click', () => {
+      const currentTheme = htmlElement.getAttribute('data-theme');
+      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+      htmlElement.setAttribute('data-theme', newTheme);
+      localStorage.setItem('theme', newTheme);
+    });
